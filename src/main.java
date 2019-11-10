1d20a09307457fe3ca91e73ef060c6c40a81fad6
Andrew Chen <andrewchen0028@gmail.com>
2019-11-10 14:15:01 -0500

    yeet

--- src/main.java
+++ src/main.java
@@ -1,8 +1,21 @@
+import krpc.client.Connection;
+import krpc.client.RPCException;
+import krpc.client.Stream;
+import krpc.client.StreamException;
+import krpc.client.services.SpaceCenter;
+
+import java.io.IOException;
+
+import java.util.concurrent.TimeUnit;
+
 public class main {
 
-    public static void main (String args[]){
-        System.out.println("Hello World!");
-        System.out.println("yeet");
+    public static void main (String args[]) throws IOException, RPCException, StreamException, InterruptedException {
+        Connection connection = Connection.newInstance("connection", "10.192.30.138", 50000, 50001);
+        SpaceCenter spaceCenter = SpaceCenter.newInstance(connection);
+        SpaceCenter.Vessel vessel = spaceCenter.getActiveVessel();
+        vessel.getControl().setThrottle(1);
+        TimeUnit.SECONDS.sleep(5);
+        vessel.getControl().activateNextStage();
+        }
     }
-
-}
