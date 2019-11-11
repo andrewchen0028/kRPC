9b5deb4373719ba70044d20221812ce4f690e7be
unknown <andrewchen0028@gmail.com>
2019-11-11 16:14:42 -0500

    thrustStream

--- src/main.java
+++ src/main.java
@@ -6,17 +6,22 @@ import krpc.client.services.SpaceCenter;
 
 import java.io.IOException;
 
+import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
 public class main {
 
     public static void main (String args[]) throws IOException, RPCException, StreamException, InterruptedException {
-        Connection connection = Connection.newInstance("connection", "10.192.30.138", 50000, 50001);
+        //Connection connection = Connection.newInstance("connection", "10.192.30.138", 50000, 50001);
+        Connection connection = Connection.newInstance();
         SpaceCenter spaceCenter = SpaceCenter.newInstance(connection);
         SpaceCenter.Vessel vessel = spaceCenter.getActiveVessel();
-        vessel.getControl().setThrottle(1);
-        TimeUnit.SECONDS.sleep(5);
-        vessel.getControl().activateNextStage();
-        System.out.println("yo");
+
+        Stream<Float> thrustStream = connection.addStream(vessel, "getThrust");
+
+        while (true) {
+            System.out.println(thrustStream.get());
+            TimeUnit.MILLISECONDS.sleep(250);
+        }
         }
     }
