b57e6fba77a217e30a4801f5787ae9f1577897e6
unknown <andrewchen0028@gmail.com>
2019-11-11 17:27:02 -0500

    launch

--- src/main.java
+++ src/main.java
@@ -1,27 +1,65 @@
 import krpc.client.Connection;
 import krpc.client.RPCException;
-import krpc.client.Stream;
+import krpc.client.Event;
 import krpc.client.StreamException;
+import krpc.client.services.KRPC;
+import krpc.client.services.KRPC.Expression;
 import krpc.client.services.SpaceCenter;
+import krpc.client.services.SpaceCenter.*;
+import krpc.schema.KRPC.ProcedureCall;
 
 import java.io.IOException;
 
-import java.sql.Time;
-import java.util.concurrent.TimeUnit;
 
 public class main {
 
-    public static void main (String args[]) throws IOException, RPCException, StreamException, InterruptedException {
-        //Connection connection = Connection.newInstance("connection", "10.192.30.138", 50000, 50001);
+    public static void main (String args[]) throws IOException, RPCException, StreamException {
+        /*
+        Connection connection = Connection.newInstance("connection", "10.192.30.138", 50000, 50001);
+         */
         Connection connection = Connection.newInstance();
+        KRPC krpc = KRPC.newInstance(connection);
         SpaceCenter spaceCenter = SpaceCenter.newInstance(connection);
-        SpaceCenter.Vessel vessel = spaceCenter.getActiveVessel();
+        Vessel vessel = spaceCenter.getActiveVessel();
 
-        Stream<Float> thrustStream = connection.addStream(vessel, "getThrust");
+        float maxThrust = 0.95f*vessel.getMaxThrust();
+        Boolean launched;
 
-        while (true) {
-            System.out.println(thrustStream.get());
-            TimeUnit.MILLISECONDS.sleep(250);
+        ProcedureCall thrust = connection.getCall(vessel, "getThrust");
+
+        Expression fullThrust = Expression.greaterThan(
+                connection,
+                Expression.call(connection, thrust),
+                Expression.constantFloat(connection, maxThrust));
+
+        Expression zeroThrust = Expression.equal(
+                connection,
+                Expression.call(connection, thrust),
+                Expression.constantFloat(connection, 0f));
+
+        System.out.println("Ignition");
+        vessel.getControl().setThrottle(1);
+        vessel.getControl().activateNextStage();
+
+        Event liftoff = krpc.addEvent(fullThrust);
+        Event separation = krpc.addEvent(zeroThrust);
+
+        synchronized (liftoff.getCondition()) {
+            liftoff.waitFor();
+            System.out.println("Liftoff");
+            vessel.getControl().activateNextStage();
+            launched = true;
         }
+
+        synchronized (separation.getCondition()) {
+            separation.waitFor();
+            if (launched) {
+                System.out.println("Separation");
+                vessel.getControl().activateNextStage();
+            }
+        }
+
+        System.out.println("Done");
+
         }
     }
