f379937876ecd922b3c60c92a28b3cfd1debc0da
Andrew Chen <andrewchen0028@gmail.com>
2019-12-03 13:29:49 -0500

    showing code to spb

--- src/main.java
+++ src/main.java
@@ -19,27 +19,24 @@ public class main {
     public static void main(String args[]) throws IOException, RPCException, InterruptedException {
 
         // Connection block
-        Connection connection = Connection.newInstance();
+//        Connection connection = Connection.newInstance();
 
-/*        Connection connection = Connection.newInstance(
+        Connection connection = Connection.newInstance(
                 "connection",
-                "10.192.24.90",
+                "10.186.149.94",
                 50000,
-                50001);*/
+                50001);
 
         // Initialize KRPC, SpaceCenter, and Vessel
         KRPC krpc = KRPC.newInstance(connection);
         SpaceCenter spaceCenter = SpaceCenter.newInstance(connection);
         Vessel vessel = spaceCenter.getActiveVessel();
 
-        float pitch = 90;
         float heading = 90;
-        double UT0 = spaceCenter.getUT();
         String fuel = "Kerosene";
         String oxidizer = "LqdOxygen";
         double[] constants = {110, 70, 0.125, -1.5}; // steering constants
 
-        vessel.getAutoPilot().targetPitchAndHeading(pitch, heading);
         vessel.getAutoPilot().engage();
         vessel.getControl().setThrottle(1);
 
@@ -47,22 +44,24 @@ public class main {
         while (vessel.getThrust() < 0.95 * vessel.getMaxThrust()) {
             TimeUnit.MILLISECONDS.sleep(10);
         }
+
         Operations.stage(vessel);
+        double UT0 = spaceCenter.getUT();
 
-        System.out.println("Stages: " + Operations.countStages(vessel));
-        System.out.println("Dstages: " + Operations.countDecoupleStages(vessel));
-
+        System.out.println("Stages: " + Operations.countDecoupleStages(vessel));
         int stage = Operations.countDecoupleStages(vessel);
 
         while (vessel.resourcesInDecoupleStage(stage - 1, false).amount(fuel) > 1
-                && vessel.resourcesInDecoupleStage(stage - 1, false).amount(oxidizer) >1) {
+                && vessel.resourcesInDecoupleStage(stage - 1, false).amount(oxidizer) > 1) {
 
-            pitch = (float) (constants[0] - constants[1] * Math.tanh(constants[2]
-                    * (vessel.flight(vessel.getReferenceFrame()).getMeanAltitude()/1000 - constants[3])));
-
+            float pitch = (float) (constants[0] - constants[1] * Math.tanh(constants[2]
+                    * (vessel.flight(vessel.getReferenceFrame()).getMeanAltitude() / 1000 - constants[3])));
             vessel.getAutoPilot().targetPitchAndHeading(pitch, heading);
-            System.out.printf("Pitch: %6.3f\n", pitch);
+
+            double time = spaceCenter.getUT() - UT0;
+            System.out.printf("MET: %6.2f   Pitch: %4.2f\n", time, pitch);
         }
+
         Operations.stage(vessel, stage);
 
         connection.close();
