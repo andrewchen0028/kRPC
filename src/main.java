da1cad1bb393c20cc6072dbf2bde18a5f92128e8
Andrew Chen <andrewchen0028@gmail.com>
2019-12-03 14:04:59 -0500

    yeeting

--- src/main.java
+++ src/main.java
@@ -32,13 +32,17 @@ public class main {
         SpaceCenter spaceCenter = SpaceCenter.newInstance(connection);
         Vessel vessel = spaceCenter.getActiveVessel();
 
+        float pitch = 90;
         float heading = 90;
         String fuel = "Kerosene";
         String oxidizer = "LqdOxygen";
-        double[] constants = {110, 70, 0.125, -1.5}; // steering constants
+        double[] constants = {95, 55, 0.085, -1.5}; // steering constants
+        double time;
 
         vessel.getAutoPilot().engage();
         vessel.getControl().setThrottle(1);
+        vessel.getAutoPilot().targetPitchAndHeading(pitch, heading);
+        vessel.getAutoPilot().setTargetRoll(0);
 
         Operations.stage(vessel); // ignition
         while (vessel.getThrust() < 0.95 * vessel.getMaxThrust()) {
@@ -51,20 +55,29 @@ public class main {
         System.out.println("Stages: " + Operations.countDecoupleStages(vessel));
         int stage = Operations.countDecoupleStages(vessel);
 
-        while (vessel.resourcesInDecoupleStage(stage - 1, false).amount(fuel) > 1
-                && vessel.resourcesInDecoupleStage(stage - 1, false).amount(oxidizer) > 1) {
-
-            float pitch = (float) (constants[0] - constants[1] * Math.tanh(constants[2]
-                    * (vessel.flight(vessel.getReferenceFrame()).getMeanAltitude() / 1000 - constants[3])));
-            vessel.getAutoPilot().targetPitchAndHeading(pitch, heading);
-
-            double time = spaceCenter.getUT() - UT0;
-            System.out.printf("MET: %6.2f   Pitch: %4.2f\n", time, pitch);
+        while (vessel.flight(vessel.getReferenceFrame()).getSurfaceAltitude() < 100) {
+            time = spaceCenter.getUT() - UT0;
+            System.out.printf("MET: %6.2f   Pitch: %4.2f   Initial Ascent\n", time, pitch);
         }
 
-        Operations.stage(vessel, stage);
+        while (vessel.resourcesInDecoupleStage(stage - 1, false).amount(fuel) > 10000
+                && vessel.resourcesInDecoupleStage(stage - 1, false).amount(oxidizer) > 17500) {
 
-        connection.close();
+            pitch = (float) (constants[0] - constants[1] * Math.tanh(constants[2]
+                        * (vessel.flight(vessel.getReferenceFrame()).getMeanAltitude() / 1000 - constants[3])));
+            vessel.getAutoPilot().targetPitchAndHeading(pitch, heading);
+
+            time = spaceCenter.getUT() - UT0;
+            System.out.printf("MET: %6.2f   Pitch: %4.2f   Gravity Turn\n", time, pitch);
+        }
+
+        // MECO, staging, MVac ignition, S2 targeting
+        vessel.getControl().setThrottle(0);
+        vessel.getAutoPilot().engage();
+        Operations.stage(vessel, stage);
+        vessel.getAutoPilot().targetPitchAndHeading(3, heading);
+        vessel.getAutoPilot().setTargetRoll(0);
+        vessel.getControl().setThrottle(1);
     }
 
 }
